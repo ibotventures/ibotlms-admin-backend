31cc442f68c23a16e5095dfa321fb18eaf9aabf1
GIBSONGODSAN <gibson.25cs@licet.ac.in>
2025-02-09 12:18:01 +0530

    v0902p

--- ibot_lms/ibot_lms/wsgi.py
+++ ibot_lms/ibot_lms/wsgi.py
@@ -3,6 +3,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibot_lms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibot_lms.ibot_lms.settings')
 
 application = get_wsgi_application()
